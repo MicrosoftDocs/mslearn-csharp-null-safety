4c7376f1b7be3b0e570f616f09bbeb7f51f0db56
David Pine <david.pine@microsoft.com>
2022-01-10 11:04:37 -0600

    Fix bug

--- src/ContosoPizza.Service/Program.cs
+++ src/ContosoPizza.Service/Program.cs
@@ -17,6 +17,15 @@ pizza.Toppings.Add(PizzaTopping.Sausage);
 pizza.Toppings.Add(PizzaTopping.Pepperoni);
 pizza.Toppings.Add(PizzaTopping.Bacon);
 pizza.Toppings.Add(PizzaTopping.Ham);
-pizza.Toppings.Add(PizzaTopping.Meatball);
+pizza.Toppings.Add(PizzaTopping.Meatballs);
 
 Console.WriteLine(pizza);
+
+/*
+    Output:
+
+    The "Meat Lover's Special" is a medium crust deep dish pizza with marinara sauce.
+    It's covered with mozzarella and parmesan cheese.
+    It comes with sausage, pepperoni, bacon, ham, and meatballs.
+    The medium size is $17.99. Delivery is $2.50 more, bringing your total $19.49!
+*/
