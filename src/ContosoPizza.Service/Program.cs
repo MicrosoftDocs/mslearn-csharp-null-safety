0149f9dc488b206ffbac544e552820c8f6afe10b
David Pine <david.pine@microsoft.com>
2022-01-26 12:55:42 -0600

    Reintroduce error and update output

--- src/ContosoPizza.Service/Program.cs
+++ src/ContosoPizza.Service/Program.cs
@@ -10,12 +10,10 @@ Pizza pizza = new("Meat Lover's Special")
 };
 
 // Add cheeses
-pizza.Cheeses = new List<PizzaCheese>();
 pizza.Cheeses.Add(PizzaCheese.Mozzarella);
 pizza.Cheeses.Add(PizzaCheese.Parmesan);
 
 // Add toppings
-pizza.Toppings = new List<PizzaTopping>();
 pizza.Toppings.Add(PizzaTopping.Sausage);
 pizza.Toppings.Add(PizzaTopping.Pepperoni);
 pizza.Toppings.Add(PizzaTopping.Bacon);
@@ -25,10 +23,10 @@ pizza.Toppings.Add(PizzaTopping.Meatballs);
 Console.WriteLine(pizza);
 
 /*
-    Output:
+    Expected output:
 
-    The "Meat Lover's Special" is a medium crust deep dish pizza with marinara sauce.
-    It's covered with mozzarella and parmesan cheese.
-    It comes with sausage, pepperoni, bacon, ham, and meatballs.
+    The "Meat Lover's Special" is a deep dish pizza with marinara sauce.
+    It's covered with a blend of mozzarella and parmesan cheese.
+    It's layered with sausage, pepperoni, bacon, ham and meatballs.
     The medium size is $17.99. Delivery is $2.50 more, bringing your total $20.49!
 */
